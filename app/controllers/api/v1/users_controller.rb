77d82dd83900c479121e470bb4bc7663131a6851
Arika Turing <ablenker@gmail.com>
2021-05-14 17:24:17 -0600

    Add validate params to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,22 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :validate_params
+
   def create
     user = User.create!(user_params)
-    render json: UserSerializer.new(user), status: :created
+    if user.save
+      render json: UserSerializer.new(user), status: :created
+    else
+      render json: { error: user.errors.full_messages.to_sentance }, status: :bad_request
+    end
   end
 
   private
 
   def user_params
     params.permit(:username, :email)
+  end
+
+  def validate_params
+    render json: { error: 'Must provide request body' }, status: :bad_request if request.body.read.blank?
   end
 end
