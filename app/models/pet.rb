fc3b5b656db95bddae65b3e3f87995d492a37661
Arika Turing <ablenker@gmail.com>
2021-05-13 19:35:30 -0600

    Add validations to pet model and spec

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,17 @@
 class Pet < ApplicationRecord
+  belongs_to :user
+  has_many :favorites
+  has_many :applications
 
-end 
+  validates_presence_of :name,
+                        :type,
+                        :description,
+                        :gender,
+                        :photo_url_1
+
+  validates :age, presence: true, numericality: {
+            greater_than_or_equal_to: 0
+          }
+
+ enum type: [:other, :dog, :cat]
+end
