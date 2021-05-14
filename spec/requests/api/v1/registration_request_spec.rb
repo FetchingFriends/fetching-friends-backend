c359c122bb0b0e60b22482c9a0545a4afdd5dee5
Arika Turing <ablenker@gmail.com>
2021-05-14 17:16:39 -0600

    Add json testing to user happy path spec

--- spec/requests/api/v1/registration_request_spec.rb
+++ spec/requests/api/v1/registration_request_spec.rb
@@ -16,6 +16,13 @@ RSpec.describe 'User registration request' do
       expect(response).to be_successful
       expect(response.status).to eq(201)
       expect(response.content_type).to eq("application/json")
+      expect(new_user_json).to have_key(:data)
+      expect(new_user_json[:data].keys).to eq([:id, :type, :attributes])
+      expect(new_user_json[:data][:id]).to eq(new_user.id.to_s)
+      expect(new_user_json[:data][:attributes].count).to eq(2)
+      expect(new_user_json[:data][:attributes].keys).to eq([:username, :email])
+      expect(new_user_json[:data][:attributes][:username]).to eq(new_user.username)
+      expect(new_user_json[:data][:attributes][:email]).to eq(new_user.email)
     end
   end
 end
