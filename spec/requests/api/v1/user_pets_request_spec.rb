ed38d7a121bd09607a86021d5be7039cd492dd2d
Jacob Arellano <jmm.arellano@gmail.com>
2021-05-15 15:23:51 -0600

    Initial commit

---
+++ spec/requests/api/v1/user_pets_request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'User registration request' do
+  describe 'happy path' do
+    it "can find a user's pets" do
+    end
+  end
+
+  describe 'sad path' do
+    it "will return an empty object if no pets are available" do
+
+    end
+
+    it "will return an error if user does not exist" do 
+
+    end
+  end
+end
