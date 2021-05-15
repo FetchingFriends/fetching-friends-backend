c160eb6c7513c0dae314cf71f3db762d74bafea1
Arika Turing <ablenker@gmail.com>
2021-05-15 15:19:41 -0600

    Add pet serializer

---
+++ app/serializers/pet_serializer.rb
@@ -0,0 +1,4 @@
+class PetSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :user_id, :name, :age, :type, :breed, :description, :gender, :fixed, :house_trained, :good_with_kids, :good_with_animals, :photo_url_1, :photo_url_2, :photo_url_3
+end
