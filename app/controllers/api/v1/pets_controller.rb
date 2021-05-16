19a678e0fbe022ad1b48a528fd098c8144311d2a
Arika Turing <ablenker@gmail.com>
2021-05-16 16:10:31 -0600

    Adjust routes

---
+++ app/controllers/api/v1/pets_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::PetsController < ApplicationController
+  def create
+    pet = Pet.create(pet_params)
+    if pet.save
+      render json: PetSerializer.new(pet), status: :created
+    end
+  end
+
+  private
+
+  def pet_params
+    params.permit(:user_id, :name, :age, :type, :breed, :description, :gender, :fixed, :house_trained, :good_with_kids, :good_with_animals, :photo_url_1, :photo_url_2, :photo_url_3)
+  end
+end
