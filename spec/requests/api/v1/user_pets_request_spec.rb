c5a013005fe59236c67bd2f960086166dedaa779
Jacob Arellano <jmm.arellano@gmail.com>
2021-05-17 13:05:07 -0600

    Map out assertions"

--- spec/requests/api/v1/user_pets_request_spec.rb
+++ spec/requests/api/v1/user_pets_request_spec.rb
@@ -3,16 +3,18 @@ require 'rails_helper'
 RSpec.describe 'User registration request' do
   describe 'happy path' do
     it "can find a user's pets" do
+      get '/api/v1/users/10/pets'
     end
   end
 
   describe 'sad path' do
     it "will return an empty object if no pets are available" do
-
+      user = create(:user)
+      get "/api/v1/users/#{user.id}/pets"
     end
 
-    it "will return an error if user does not exist" do 
-
+    it "will return an error if user does not exist" do
+      get '/api/v1/users/10/pets'
     end
   end
 end
