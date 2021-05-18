b8a7d33946b067c355446709f2ec27c8898625c0
Jacob Arellano <jmm.arellano@gmail.com>
2021-05-17 18:29:50 -0600

    Add class methods for pet search endpoint

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -14,4 +14,34 @@ class Pet < ApplicationRecord
           }
 
   enum pet_type: [:other, :dog, :cat]
+
+  def self.pet_search(search_term)
+    enum = enum_eval(search_term)
+    if enum
+      Pet.where("pet_type = ?", enum)
+    else
+      false
+    end
+  end
+
+  def self.enum_eval(search_term)
+    search_term = check_plural?(search_term) if !search_term.nil?
+    if search_term == "other"
+      0
+    elsif search_term == "dog"
+      1
+    elsif search_term == "cat"
+      2
+    else
+      false
+    end
+  end
+
+  def self.check_plural?(search_term)
+    if search_term.downcase[-1] == 's'
+      search_term = search_term[0..-2].downcase
+    else
+      search_term = search_term.downcase
+    end
+  end
 end
